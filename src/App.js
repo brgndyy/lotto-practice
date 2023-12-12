f81dbdec9b6882dae6d407c1813a121d732762f0
brgndy <brgndy96@gmail.com>
2023-12-12 14:30:44 +0900

    Fix: main 브랜치와 병합

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import OutputView from './views/OutputView.js';
 import Lotto from './Lotto.js';
+=======
+import asyncFunctionHandlerWithError from './utils/asyncFunctionHandlerWithError.js';
+import OutputView from './views/OutputView.js';
+import Lotto from './Lotto.js';
+import InputView from './views/InputView.js';
+import BonusNumberValidator from './validator/BonusNumberValidator.js';
+>>>>>>> main
 
 class App {
   #winningLottoNumbers;
@@ -12,11 +20,24 @@ class App {
 
   async play() {
     this.#printWinnigLottoNumbersExceptBonusNumber();
+<<<<<<< HEAD
+=======
+    await asyncFunctionHandlerWithError(this.#generateWinningLotto, this);
+>>>>>>> main
   }
 
   #printWinnigLottoNumbersExceptBonusNumber() {
     OutputView.printWinningLottoNumbersExceptBonus(this.#winningLottoNumbers);
   }
+<<<<<<< HEAD
+=======
+
+  async #generateWinningLotto() {
+    this.#bonusNumber = await InputView.readBonusNumber();
+
+    BonusNumberValidator.validateBonusNumber(this.#winningLottoNumbers, this.#bonusNumber);
+  }
+>>>>>>> main
 }
 
 export default App;
